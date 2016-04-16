36be7b1124de2dcfeb508e24ac152948680fcf68
Justas Palumickas <jpalumickas@gmail.com>
2016-04-16 15:37:06 +0300

    Change SimpleCov formatters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
+
 SimpleCov.start
 
 require 'ivapi'
