679d9599a287a2954298d2bf6eb327673ae6d716
Justas Palumickas <jpalumickas@gmail.com>
2017-06-07 20:15:23 +0300

    Update spec to test respond_to_missing?

--- spec/ivapi_spec.rb
+++ spec/ivapi_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe Ivapi do
   it 'responds to config' do
-    expect(Ivapi.respond_to?(:configure)).to be_truthy
+    expect(Ivapi.respond_to?(:account)).to be_truthy
   end
-
-  # describe '.new' do
-  #   it 'is a Ivapi::Client' do
-  #     expect(Ivapi.new).to be_a Ivapi::Client
-  #   end
-  # end
 end
